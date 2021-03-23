17c763a1e3af26e93f003050ab19125f98eae24a
Yandi Farinango <farinango.yandi@gmail.com>
2021-03-23 15:30:59 -0500

    Scraped CNN, WallstreetBets, YahooFinance

---
+++ News Sources Scraped/cnn.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import time
+import pycurl
+from pathlib import Path
+from io import BytesIO
+
+
+
+def main():
+    buf = BytesIO()
+    crl = pycurl.Curl()
+
+    # Set URL value
+    crl.setopt(crl.URL, 'https://www.cnn.com/business')
+
+    # Write bytes that are utf-8 encoded
+    crl.setopt(crl.WRITEDATA, buf)
+
+    # Perform a file transfer
+    crl.perform()
+
+    # End curl session
+    crl.close()
+
+    # Get the content stored in the BytesIO object (in byte characters)
+    body = buf.getvalue()
+
+
+    # Populate .../data subdirectory
+    # If subdirectory doesnt exist, create folder
+    Path("C:/Users/farin/PycharmProjects/Automated-Sentiment-Analysis/bitcoin scrape/scraper/data/cnn").mkdir(parents=True, exist_ok=True)
+
+
+    # Decode the bytes stored in get_body to HTML and print the result
+    # print('Output of GET request:\n%s' % get_body.decode('utf8'))
+    fname = "C:/Users/farin/PycharmProjects/Automated-Sentiment-Analysis/bitcoin scrape/scraper/data/cnn/" + str(int(time.time())) + ".txt"
+    with open(fname, "wb") as outfile:
+        outfile.write(body)
+
+if __name__ == "__main__":
+    main()
+
